ea2665c29c860b704435cbc0080c75cc87c7edd1
gayanes <gayane.sance28@gmail.com>
2019-09-09 13:48:02 -0500

    COMP 127 Basic Java Activities, Part 1

---
+++ src/basicjava/BasicStuff.java
@@ -0,0 +1,41 @@
+package basicjava;
+
+public class BasicStuff {
+    public static void main(String[] args){
+//        int age0 = 18;
+//        int age1 = 20;
+//        double age0 = 18;
+//        double age1 = 20;
+//        System.out.println("We are " + age0 + " and " + age1 + " years old!");
+
+//        int sumOfAges = age0 + age1;
+//        System.out.println("The sum of our ages is " + sumOfAges + "!");
+//        System.out.println("The sum of our ages is " + (age0 + age1) + "!");
+//        A programmer might forget that concatenation happens before addition.
+
+//        System.out.println(6 / 3);
+//        System.out.println(6 / 4);
+//        Because both 6 and 4 are integers, Java does integer division and gets rid of the decimal.
+//        System.out.println(6 / 4.0);
+
+//        System.out.println(Integer.MAX_VALUE);
+//        System.out.println(Integer.MIN_VALUE);
+//        Integer.MIN_VALUE is the lowest integer value Java can produce. It contains the most number of bits and is negative.
+//        System.out.println(Integer.MIN_VALUE - 1);
+//        If all the integer values were stored in a list, the index of Integer.MAX_VALUE would be the index before Integer.MIN_VALUE
+
+//        int numOfHours = 1600;
+//        int numOfDays = numOfHours / 24;
+//        numOfHours -= numOfDays * 24;
+//        int numOFWeeks = numOfHours / 7;
+//        numOfDays -= numOFWeeks * 7;
+//        System.out.println("1600 hours is " + numOFWeeks + " weeks, " + numOfDays + " days, and " + numOfHours + " hours.");
+
+//        double h = 5;
+//        double r = 2;
+//        double V = (1 / 3.0) * Math.PI * Math.pow(r, 2) * h;
+//        double A = Math.PI * Math.pow(r, 2) + Math.PI * r * Math.pow((Math.pow(r, 2) + Math.pow(h, 2)), 0.5);
+//        System.out.println("Volume: " + V);
+//        System.out.println("Surface Area: " + A);
+    }
+}
